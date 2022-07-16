2bd01b4997a25c3fbc3ea210cc8c491879a23462
longdhn <longdhn.lk@gmail.com>
2022-07-16 18:24:12 +0700

    Collection

--- LearningCollection/Program.cs
+++ LearningCollection/Program.cs
@@ -12,20 +12,33 @@ arrayList1.Add("Item 2");
 arrayList1.Add("Item 3");
 arrayList1.Add("Item 4"); //Item 1, Item 2, Item 3, Item 4
 
-arrayList2.Add("Item ArrayList 2"); //Item ArrayList 2
-arrayList1.AddRange(arrayList2); // Item 1, Item 2, Item 3, Item 4, Item ArrayList 2
+//arrayList2.Add("Item ArrayList 2"); //Item ArrayList 2
+//arrayList1.AddRange(arrayList2); // Item 1, Item 2, Item 3, Item 4, Item ArrayList 2
 
-arrayList1.Insert(2, "New Item"); // Item 1, Item 2, "New Item" ,Item 3, Item 4, Item ArrayList 2
+//arrayList1.Insert(2, "New Item"); // Item 1, Item 2, "New Item" ,Item 3, Item 4, Item ArrayList 2
 
-for (var i = 0; i < arrayList1.Count; i++)
-{
-    Console.WriteLine(arrayList1[i]);
-}
+//for (var i = 0; i < arrayList1.Count; i++)
+//{
+//    Console.WriteLine(arrayList1[i]);
+//}
+
+//Clone(): tạo bản sao cho ArrayList
+ArrayList arrayList3 = (ArrayList)arrayList1.Clone(); 
+//tham chiếu (reference type)
+arrayList3[0] = "Item 1 updated";
+
+
+Console.WriteLine(arrayList1[0]); //Item 1
+
+
+
 
 Console.WriteLine("===================2. HashTable ====================");
 Hashtable hashtable = new Hashtable();
 hashtable.Add("Key1", "Value1");
 hashtable.Add("Key2", "Value2");
+hashtable.Add("Key3", "Value3");
+hashtable.Add("Key4", "Value4");
 Console.WriteLine(hashtable["Key1"]);
 
 
@@ -43,6 +56,56 @@ foreach (var key in hashtable.Keys)
 foreach (var value in hashtable.Values)
 {
     Console.WriteLine("Value: {0} ", value);
+}
+
+
+//bảng xếp hạng
+Console.WriteLine("===================3. Sorted List ====================");
+
+SortedList ranking = new SortedList();
+ranking.Add("4", "Peter");
+ranking.Add("3", "Tony stark");
+ranking.Add("5", "Tom");
+
+for (int i = 0; i < ranking.Count; i++)
+{
+    Console.WriteLine("\t{0}:\t{1}", ranking.GetKey(i), ranking.GetByIndex(i));
+}
+
+Console.WriteLine("===================4. Stack ====================");
+Stack myStack = new Stack();
+myStack.Push("Hello");
+myStack.Push("World");
+myStack.Push("!");
+// Displays the properties and values of the Stack.
+Console.WriteLine("myStack");
+Console.WriteLine("\tCount: {0}", myStack.Count);
+Console.Write("\tValues:");
+foreach (Object obj in myStack)
+    Console.Write(" {0}", obj);
+
+
+Console.WriteLine("===================5. Queue ====================");
+// Creates and initializes a new Queue.
+Queue myQ = new Queue();
+myQ.Enqueue("Hello");
+myQ.Enqueue("World");
+myQ.Enqueue("!");
+// Displays the properties and values of the Queue.
+Console.WriteLine("myQ");
+Console.WriteLine("\tCount: {0}", myQ.Count);
+Console.Write("\tValues:");
+foreach (Object obj in myQ)
+    Console.Write(" {0}", obj);
+
+Console.WriteLine("===================6. List ====================");
+List<int> list1 = new List<int>();
+list1.Add(1);
+//list1.Add("Pony"); //<-- Error at compile process
+int total = 0;
+foreach (int num in list1)
+{
+    total += num;
 }
 
 Console.ReadLine();
