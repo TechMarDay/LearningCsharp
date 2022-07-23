014c1d37a5480346dc89f182af53c57015779e6e
DESKTOP-IV277RF\long-pc <Nguyenlong1002@gmail.com>
2022-07-23 10:34:49 +0700

    project Arr

---
+++ ProjectArray/Program.cs
@@ -0,0 +1,9 @@
+﻿
+Console.OutputEncoding = System.Text.Encoding.UTF8;
+Console.WriteLine("2.1 Viết hàm đảo ngược 2 phần tử trong mảng theo vị trí.");
+Console.WriteLine("2.1 Viết hàm đảo ngược 2 phần tử trong mảng theo vị trí.");
+Console.WriteLine("2.3 Đếm số lượng giá trị lớn nhất có trong mảng");
+
+Array arr = new Array();
+
+
