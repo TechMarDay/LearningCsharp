118e123fbb2f86014f2ae4a69f1ad4fbc6a16499
longdhn <longdhn.lk@gmail.com>
2022-09-02 10:42:15 +0700

    Remove unused comments

--- EFCoreDBFirst/Models/EmployeeManagementContext.cs
+++ EFCoreDBFirst/Models/EmployeeManagementContext.cs
@@ -26,7 +26,6 @@ namespace EFCoreDBFirst.Models
         {
             if (!optionsBuilder.IsConfigured)
             {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
                 optionsBuilder.UseSqlServer("Data Source=.\\SQLEXPRESS;Initial Catalog=EmployeeManagement;User ID=sa;Password=Asdf@123456;Connect Timeout=30;Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False");
             }
         }
